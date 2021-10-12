d894210436040431e05555f8709e46f4b86ebf57
CarlosOliveira1988 <CarlosOliveira1988@users.noreply.github.com>
2021-10-12 19:00:47 -0300

    Converting the 'dataframe_filter_test.py' to a class structure
    Converting the 'dataframe_filter_test.py' to a class structure, in order to improve the code organization

--- indexer_lib/dataframe_filter_test.py
+++ indexer_lib/dataframe_filter_test.py
@@ -7,209 +7,220 @@ import pytest
 
 from indexer_lib.dataframe_filter import DataframeFilter
 
-"""Tuple objects for testing purpose: dataframe + filter."""
+
+class ObjectsForTesting:
+    """Tuple objects for testing purpose: dataframe + filter."""
+
+    def getTestObjects(self):
+        """Return a dataframe and a DataframeFilter for testing purpose."""
+        start = "2021-10-01"
+        end = "2021-10-06"
+        table_dict = {
+            "col1": range(6),
+            "col2": range(6, 12),
+            "date": pd.date_range(start=start, end=end).tolist(),
+        }
+        return pd.DataFrame(data=table_dict), DataframeFilter()
+
+    def fullTestDataframe(self):
+        """Return a full test dataframe."""
+        df, filter = self.getTestObjects()
+        return df, filter
+
+    def nonCol1TestDataframe(self):
+        """Return a non-Col1 test dataframe."""
+        df, filter = self.getTestObjects()
+        df.drop(["col1"], axis="columns", inplace=True)
+        return df, filter
+
+    def nonCol2TestDataframe(self):
+        """Return a non-Col2 test dataframe."""
+        df, filter = self.getTestObjects()
+        df.drop(["col2"], axis="columns", inplace=True)
+        return df, filter
+
+    def nonDateTestDataframe(self):
+        """Return a non-Date test dataframe."""
+        df, filter = self.getTestObjects()
+        df.drop(["date"], axis="columns", inplace=True)
+        return df, filter
+
+    def emptyTestDataframe(self):
+        """Return a empty test dataframe."""
+        df, filter = self.getTestObjects()
+        df.drop(["col1", "col2", "date"], axis="columns", inplace=True)
+        return df, filter
+
+    def nonDataframe(self):
+        """Return a non-dataframe type."""
+        df, filter = self.getTestObjects()
+        df = int()
+        return df, filter
 
 
-def getTestObjects():
-    """Return a dataframe and a DataframeFilter objects for testing purpose."""
-    table_dict = {
-        "col1": range(6),
-        "col2": range(6, 12),
-        "date": pd.date_range(start="2021-10-01", end="2021-10-06").tolist(),
-    }
-    return pd.DataFrame(data=table_dict), DataframeFilter()
+class Test_filterDataframePerColumn:
+    """Tests for 'filterDataframePerColumn' method."""
+
+    obj_for_testing = ObjectsForTesting()
+
+    # List of tuples, with the following order per tuple:
+    # - dft, filter
+    validValues_list = [
+        obj_for_testing.fullTestDataframe(),
+        obj_for_testing.nonCol1TestDataframe(),
+        obj_for_testing.nonCol2TestDataframe(),
+        obj_for_testing.nonDateTestDataframe(),
+        obj_for_testing.emptyTestDataframe(),
+    ]
+
+    @pytest.mark.parametrize("dft, filter", validValues_list)
+    def test_validValues(self, dft, filter):
+        """Test the 'filterDataframePerColumn' method against dataframes.
+
+        The application will not brake if some column is not found.
+        """
+        fdf = filter.filterDataframePerColumn(dft, "col1", 0)
+        assert isinstance(fdf, pd.DataFrame) is True
+
+    def test_nonValidValues(self):
+        """Test the 'filterDataframePerColumn' method against non-dataframes.
+
+        An exception will be raised if non-dataframe type is used.
+        """
+        obj_for_testing = ObjectsForTesting()
+        non_dft, filter = obj_for_testing.nonDataframe()
+        with pytest.raises(TypeError):
+            fdf = filter.filterDataframePerColumn(non_dft, "col1", 0)
+
+    def test_foundOK(self):
+        """Check if the filter works correctly with one valid value."""
+        obj_for_testing = ObjectsForTesting()
+        dft, filter = obj_for_testing.fullTestDataframe()
+        fdf = filter.filterDataframePerColumn(dft, "col1", 0)
+        assert len(fdf) == 1
+
+    def test_foundNOK(self):
+        """Check if the filter works correctly when no valid value is found."""
+        obj_for_testing = ObjectsForTesting()
+        dft, filter = obj_for_testing.fullTestDataframe()
+        fdf = filter.filterDataframePerColumn(dft, "col1", 1000)
+        assert len(fdf) == 0
 
 
-def fullTestDataframe():
-    """Return a full test dataframe."""
-    df, filter = getTestObjects()
-    return df, filter
+class Test_filterDataframePerPeriod:
+    """Tests for 'filterDataframePerPeriod' method."""
 
+    obj_for_testing = ObjectsForTesting()
 
-def nonCol1TestDataframe():
-    """Return a non-Col1 test dataframe."""
-    df, filter = getTestObjects()
-    df.drop(["col1"], axis="columns", inplace=True)
-    return df, filter
+    lower_invalid_date = datetime.strptime("2020/10/01", "%Y/%m/%d")
+    initial_date = datetime.strptime("2021/10/01", "%Y/%m/%d")
+    medium_date = datetime.strptime("2021/10/04", "%Y/%m/%d")
+    final_date = datetime.strptime("2021/10/06", "%Y/%m/%d")
+    upper_invalid_date = datetime.strptime("2022/10/06", "%Y/%m/%d")
 
+    # List of tuples, with the following order per tuple:
+    # - dft, filter, ini_date, end_date
+    validValues_list = [
+        obj_for_testing.fullTestDataframe() + (initial_date, final_date),
+        obj_for_testing.nonCol1TestDataframe() + (initial_date, final_date),
+        obj_for_testing.nonCol2TestDataframe() + (initial_date, final_date),
+        obj_for_testing.nonDateTestDataframe() + (initial_date, final_date),
+        obj_for_testing.emptyTestDataframe() + (initial_date, final_date),
+    ]
 
-def nonCol2TestDataframe():
-    """Return a non-Col2 test dataframe."""
-    df, filter = getTestObjects()
-    df.drop(["col2"], axis="columns", inplace=True)
-    return df, filter
+    # List of tuples, with the following order per tuple:
+    # - ini_date, end_date, expected_len
+    foundOK_dateList = [
+        (initial_date, final_date, 6),
+        (initial_date, medium_date, 4),
+        (medium_date, final_date, 3),
+        (lower_invalid_date, upper_invalid_date, 6),
+        (upper_invalid_date, lower_invalid_date, 0),
+    ]
 
-
-def nonDateTestDataframe():
-    """Return a non-Date test dataframe."""
-    df, filter = getTestObjects()
-    df.drop(["date"], axis="columns", inplace=True)
-    return df, filter
-
-
-def emptyTestDataframe():
-    """Return a empty test dataframe."""
-    df, filter = getTestObjects()
-    df.drop(["col1", "col2", "date"], axis="columns", inplace=True)
-    return df, filter
-
-
-def nonDataframe():
-    """Return a non-dataframe type."""
-    df, filter = getTestObjects()
-    df = int()
-    return df, filter
-
-
-"""Tests for 'filterDataframePerColumn' method."""
-
-
-filterDataframePerColumn_list = [
-    fullTestDataframe(),
-    nonCol1TestDataframe(),
-    nonCol2TestDataframe(),
-    nonDateTestDataframe(),
-    emptyTestDataframe(),
-]
-
-
-@pytest.mark.parametrize("dft, filter", filterDataframePerColumn_list)
-def test_filterDataframePerColumn_validValues(dft, filter):
-    """Test the 'filterDataframePerColumn' method against dataframes.
-
-    The application will not brake if some column is not found.
-    """
-    fdf = filter.filterDataframePerColumn(dft, "col1", 0)
-    assert isinstance(fdf, pd.DataFrame) is True
-
-
-def test_filterDataframePerColumn_nonValidValues():
-    """Test the 'filterDataframePerColumn' method against non-dataframes.
-
-    An exception will be raised if non-dataframe type is used.
-    """
-    non_dft, filter = nonDataframe()
-    with pytest.raises(TypeError):
-        fdf = filter.filterDataframePerColumn(non_dft, "col1", 0)
-
-
-def test_filterDataframePerColumn_foundOK():
-    """Check if the filter works correctly with one valid value."""
-    dft, filter = fullTestDataframe()
-    fdf = filter.filterDataframePerColumn(dft, "col1", 0)
-    assert len(fdf) == 1
-
-
-def test_filterDataframePerColumn_foundNOK():
-    """Check if the filter works correctly when none valid value is found."""
-    dft, filter = fullTestDataframe()
-    fdf = filter.filterDataframePerColumn(dft, "col1", 1000)
-    assert len(fdf) == 0
-
-
-"""Tests for 'filterDataframePerPeriod' method."""
-
-lower_invalid_date = datetime.strptime("2020/10/01", "%Y/%m/%d")
-initial_date = datetime.strptime("2021/10/01", "%Y/%m/%d")
-medium_date = datetime.strptime("2021/10/04", "%Y/%m/%d")
-final_date = datetime.strptime("2021/10/06", "%Y/%m/%d")
-upper_invalid_date = datetime.strptime("2022/10/06", "%Y/%m/%d")
-
-filterDataframePerPeriod_list = [
-    fullTestDataframe() + (initial_date, final_date),
-    nonCol1TestDataframe() + (initial_date, final_date),
-    nonCol2TestDataframe() + (initial_date, final_date),
-    nonDateTestDataframe() + (initial_date, final_date),
-    emptyTestDataframe() + (initial_date, final_date),
-]
-
-
-@pytest.mark.parametrize(
-    "dft, filter, ini_date, end_date", filterDataframePerPeriod_list
-)
-def test_filterDataframePerPeriod_validValues(dft, filter, ini_date, end_date):
-    """Test the 'filterDataframePerPeriod' method against dataframes.
-
-    The application will not brake if some column is not found.
-    """
-    fdf = filter.filterDataframePerPeriod(
-        dft,
-        "date",
-        ini_date,
-        end_date,
+    @pytest.mark.parametrize(
+        "dft, filter, ini_date, end_date",
+        validValues_list,
     )
-    assert isinstance(fdf, pd.DataFrame) is True
+    def test_validValues(self, dft, filter, ini_date, end_date):
+        """Test the 'filterDataframePerPeriod' method against dataframes.
 
-
-def test_filterDataframePerPeriod_nonValidValues():
-    """Test the 'filterDataframePerPeriod' method against non-dataframes.
-
-    An exception will be raised if non-dataframe type is used.
-    """
-    non_dft, filter = nonDataframe()
-    ini_date = initial_date
-    end_date = final_date
-    with pytest.raises(TypeError):
+        The application will not brake if some column is not found.
+        """
         fdf = filter.filterDataframePerPeriod(
-            non_dft,
+            dft,
             "date",
             ini_date,
             end_date,
         )
+        assert isinstance(fdf, pd.DataFrame) is True
+
+    def test_nonValidValues(self):
+        """Test the 'filterDataframePerPeriod' method against non-dataframes.
+
+        An exception will be raised if non-dataframe type is used.
+        """
+        obj_for_testing = ObjectsForTesting()
+        non_dft, filter = obj_for_testing.nonDataframe()
+        ini_date = self.initial_date
+        end_date = self.final_date
+        with pytest.raises(TypeError):
+            fdf = filter.filterDataframePerPeriod(
+                non_dft,
+                "date",
+                ini_date,
+                end_date,
+            )
+
+    @pytest.mark.parametrize(
+        "ini_date, end_date, expected_len",
+        foundOK_dateList,
+    )
+    def test_foundOK(self, ini_date, end_date, expected_len):
+        """Check if the filter works correctly with some valid interval."""
+        obj_for_testing = ObjectsForTesting()
+        dft, filter = obj_for_testing.fullTestDataframe()
+        fdf = filter.filterDataframePerPeriod(dft, "date", ini_date, end_date)
+        assert len(fdf) == expected_len
 
 
-filterDataframePerPeriod_dateList = [
-    (initial_date, final_date, 6),
-    (initial_date, medium_date, 4),
-    (medium_date, final_date, 3),
-    (lower_invalid_date, upper_invalid_date, 6),
-    (upper_invalid_date, lower_invalid_date, 0),
-]
+class Test_getListFromDataframeColumn:
+    """Tests for 'getListFromDataframeColumn' method."""
 
+    obj_for_testing = ObjectsForTesting()
 
-@pytest.mark.parametrize(
-    "ini_date, end_date, expected_len", filterDataframePerPeriod_dateList
-)
-def test_filterDataframePerPeriod_foundOK(ini_date, end_date, expected_len):
-    """Check if the filter works correctly with some valid interval."""
-    dft, filter = fullTestDataframe()
-    fdf = filter.filterDataframePerPeriod(dft, "date", ini_date, end_date)
-    assert len(fdf) == expected_len
+    # List of tuples, with the following order per tuple:
+    # - dft, filter
+    validValues_list = [
+        obj_for_testing.fullTestDataframe(),
+        obj_for_testing.nonCol1TestDataframe(),
+        obj_for_testing.nonCol2TestDataframe(),
+        obj_for_testing.nonDateTestDataframe(),
+        obj_for_testing.emptyTestDataframe(),
+    ]
 
+    @pytest.mark.parametrize("dft, filter", validValues_list)
+    def test_validValues(self, dft, filter):
+        """Test the 'getListFromDataframeColumn' method against dataframes.
 
-"""Tests for 'getListFromDataframeColumn' method."""
+        The application will not brake if some column is not found.
+        """
+        result_list = filter.getListFromDataframeColumn(dft, "col1")
+        assert isinstance(result_list, list) is True
 
-getListFromDataframeColumn_list = [
-    fullTestDataframe(),
-    nonCol1TestDataframe(),
-    nonCol2TestDataframe(),
-    nonDateTestDataframe(),
-    emptyTestDataframe(),
-]
+    def test_nonValidValues(self):
+        """Test the 'getListFromDataframeColumn' method against non-dataframes.
 
+        An exception will be raised if non-dataframe type is used.
+        """
+        obj_for_testing = ObjectsForTesting()
+        non_dft, filter = obj_for_testing.nonDataframe()
+        with pytest.raises(TypeError):
+            fdf = filter.getListFromDataframeColumn(non_dft, "col1")
 
-@pytest.mark.parametrize("dft, filter", getListFromDataframeColumn_list)
-def test_getListFromDataframeColumn_validValues(dft, filter):
-    """Test the 'getListFromDataframeColumn' method against dataframes.
-
-    The application will not brake if some column is not found.
-    """
-    result_list = filter.getListFromDataframeColumn(dft, "col1")
-    assert isinstance(result_list, list) is True
-
-
-def test_getListFromDataframeColumn_nonValidValues():
-    """Test the 'getListFromDataframeColumn' method against non-dataframes.
-
-    An exception will be raised if non-dataframe type is used.
-    """
-    non_dft, filter = nonDataframe()
-    with pytest.raises(TypeError):
-        fdf = filter.getListFromDataframeColumn(non_dft, "col1")
-
-
-def test_getListFromDataframeColumn_foundOK():
-    """Check if the filter works correctly with some valid values."""
-    dft, filter = fullTestDataframe()
-    fdf = filter.getListFromDataframeColumn(dft, "col1")
-    assert len(fdf) == 6
+    def test_foundOK(self):
+        """Check if the filter works correctly with some valid values."""
+        obj_for_testing = ObjectsForTesting()
+        dft, filter = obj_for_testing.fullTestDataframe()
+        fdf = filter.getListFromDataframeColumn(dft, "col1")
+        assert len(fdf) == 6
